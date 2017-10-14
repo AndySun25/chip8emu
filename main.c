1d761c23f15f440a2dec9b86954a1876f0205a6d
Andy <andy_sun_sha@hotmail.com>
2017-10-14 12:36:26 +0200

    Removed double sleep.

--- main.c
+++ main.c
@@ -176,7 +176,6 @@ int main(int argc, char **argv)
         {
             render();
             draw_flag = false;
-            nanosleep(&tspec, &_);
         }
         duration = (double) (clock() - loop_start) / CLOCKS_PER_SEC;
 
@@ -188,5 +187,5 @@ int main(int argc, char **argv)
     }
 
     tearDown();
-    exit(0);
+    return 0;
 }
